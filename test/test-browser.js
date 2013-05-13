a831d266ca3a53eba6bfc0c44a0fdbd55488a339
Joseph Werle <joseph.werle@gmail.com>
2013-05-13 18:52:13 -0400

    hopefully a better test for testling

--- test/test-browser.js
+++ test/test-browser.js
@@ -1,6 +1,7 @@
 // for mocha from cli
 if (typeof window === 'undefined') return false;
 
+window.leveljs = leveljs || require('level-js')
 window.model = LevelModel || require('level-model');
 var db = leveljs('mydb')
 model.set('db', db);
@@ -8,21 +9,35 @@ model.set('persist', true);
 
 var User = model('User', { name:String, age:Number })
 
-db.open(function () {
-  user = new User({name: 'werleeee'})
-  user.saveAs('werleee', function (err) { 
-    if (err) throw err;
-    user.read(function (err, data) {
+if (typeof describe === 'function' && typeof it === 'function') {
+  describe('(browser) level-model', function () {
+    it ("Should be just okay", function (done) {
+      kickbackAndGo(done)
+    });
+  });
+}
+else {
+  kickbackAndGo(false);
+}
+
+function kickbackAndGo (done) {
+  db.open(function () {
+    user = new User({name: 'werleeee'})
+    user.saveAs('werleee', function (err) { 
       if (err) throw err;
-      user.age = 22;
-      user.save(function (err) {
+      user.read(function (err, data) {
         if (err) throw err;
-        user.read(function (err, data) {
+        user.age = 22;
+        user.save(function (err) {
           if (err) throw err;
-          console.log(data.name)
-          console.log(data.age)
+          user.read(function (err, data) {
+            if (err) throw err;
+            console.log(data.name)
+            console.log(data.age)
+            done && done();
+          })
         })
       })
-    })
+    });
   });
-});
+}
