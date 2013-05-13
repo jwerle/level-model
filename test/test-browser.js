91084fd7de7781fa5506b6748bac2e9bf5492308
Joseph Werle <joseph.werle@gmail.com>
2013-05-13 17:35:00 -0400

    seperated js file for browser test

---
+++ test/test-browser.js
@@ -0,0 +1,25 @@
+window.model = LevelModel || require('level-model');
+var db = leveljs('mydb')
+model.set('db', db);
+model.set('persist', true);
+
+var User = model('User', { name:String, age:Number })
+
+db.open(function () {
+  user = new User({name: 'werleeee'})
+  user.saveAs('werleee', function (err) { 
+    if (err) throw err;
+    user.read(function (err, data) {
+      if (err) throw err;
+      user.age = 22;
+      user.save(function (err) {
+        if (err) throw err;
+        user.read(function (err, data) {
+          if (err) throw err;
+          console.log(data.name)
+          console.log(data.age)
+        })
+      })
+    })
+  });
+});
